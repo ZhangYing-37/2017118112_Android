72ced99b8e57b162b54746759720430693feb9dc
Zhang_Ying <2017118112@stu.hstc.edu.cn>
2019-10-06 09:59:04 +0800

    活动启动模式：singleTop

--- HelloWorld/app/src/main/java/com/hstc/edu/helloworld/HelloWorld2.java
+++ HelloWorld/app/src/main/java/com/hstc/edu/helloworld/HelloWorld2.java
@@ -9,10 +9,12 @@ import android.view.View;
 import android.widget.Button;
 
 public class HelloWorld2 extends AppCompatActivity {
+    public static String TAG = "HelloWorld2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate");
         setContentView(R.layout.hello2_layout);
         Button hello1 = (Button) findViewById(R.id.Hello1);
         Button hello2 = (Button) findViewById(R.id.Hello2);
@@ -31,4 +33,40 @@ public class HelloWorld2 extends AppCompatActivity {
             }
         });
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");;
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"onRestart");
+    }
 }
+
